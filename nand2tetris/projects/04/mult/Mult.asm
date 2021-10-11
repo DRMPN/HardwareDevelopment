39b055af98cc19e6e209989f672a53e85aa9bb99
DRMPN <mr.gorden.smit@gmail.com>
2021-10-11 19:53:24 +0300

    implemented multiplication

--- nand2tetris/projects/04/mult/Mult.asm
+++ nand2tetris/projects/04/mult/Mult.asm
@@ -9,4 +9,43 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// preusocode
+// R2 = 0
+//
+// LOOP
+//    while R1 != 0
+//          R2 = R0 + R2
+//          R1 = R1 - 1
+// END
+
+
+// initialize R2 to 0
+@R2
+M=0
+
+(LOOP)
+// get R1 value
+@R1
+D=M
+
+// if D == 0 then jump to @END else do math
+@END
+D;JEQ
+
+// do math
+@R0
+D=M
+@R2
+M=D+M
+@R1
+M=M-1
+
+// go again
+@LOOP
+0;JMP
+
+
+// infinite loop
+(END)
+@END
+0;JMP
