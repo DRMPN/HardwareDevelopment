ca3be5654cbde7ea93eab4cee314766e4872793f
DRMPN <mr.gorden.smit@gmail.com>
2021-10-12 23:13:20 +0300

    implemented fil

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,85 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// ideally find a way to reduce two loops, but I can't T_T
+
+// set screen memory size
+@8191
+D = A
+@size
+M = D
+
+
+(START)
+// .........................................
+// get input from keyboard
+@KBD
+D = M
+                                        
+// if any key is pressed, jump
+@PRESSED
+D; JNE
+// .........................................
+
+// .........................................
+// do not feel the screen with black pixels
+
+// jump to screen loop
+@CLEAR_SCREEN
+0; JMP
+// .........................................
+
+// .........................................
+// fill the screen with black pixels
+
+(PRESSED)
+
+// jump to screen loop
+@FILL_SCREEN
+0; JMP
+// .........................................
+
+
+// -----------------------------------
+(CLEAR_SCREEN)
+
+@size
+D = M
+
+(LOOP_CS)
+@START
+D; JLT
+
+@SCREEN
+A = D + A
+M = 0
+D = D - 1
+
+@LOOP_CS
+0;JMP
+// -----------------------------------
+
+
+// -----------------------------------
+(FILL_SCREEN)
+
+@size
+D = M
+
+(LOOP_FS)
+@START
+D; JLT
+
+@SCREEN
+A = D + A
+M = -1
+D = D - 1
+
+@LOOP_FS
+0;JMP
+// -----------------------------------
+
+
+// end of the program, shouldn't be reachable
+(END)
+0;JMP
