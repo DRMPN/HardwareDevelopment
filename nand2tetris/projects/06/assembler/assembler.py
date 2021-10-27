0e92826a8ebc2332014264ab3477fc705fbf24c0
DRMPN <mr.gorden.smit@gmail.com>
2021-10-27 21:25:02 +0300

    implemented assembler for programs with symbols

--- nand2tetris/projects/06/assembler/assembler.py
+++ nand2tetris/projects/06/assembler/assembler.py
@@ -4,9 +4,14 @@ from typing import IO
 from model.code import Code
 from model.parser import Parser
 from model.command_type import CommandType
+from model.symbol_table import SymbolTable
 
 
 # TODO: add decorators, or at least give it a try
+
+# TODO: reforge
+
+# TODO: add predefined symbols
 
 # TODO: comment
 def main() -> IO:
@@ -19,12 +24,16 @@ def main() -> IO:
     # Reads data from an input file 
     try:
         input_file = open(sys.argv[1], 'r')
-    except FileNotFoundError:
-        sys.exit("File " + sys.argv[1] + " cannot be found")
+    except OSError:
+        sys.exit("File '" + sys.argv[1] + "' cannot be found/opened")
 
 
     # Initializes parser with data
     parser = Parser(input_file.readlines())
+
+
+    # Initialize symbol table
+    symbol_table = SymbolTable()
 
 
     # Closes input file
@@ -38,21 +47,60 @@ def main() -> IO:
         sys.exit("Output file " + sys.argv[1] + " cannot be created")
 
 
-    # Parses input data and writes it to an output file
-    # TODO: checks for a command type two times
+    ### First pass
+    # Builds symbol table
+    ROM = 0
     for i in range(parser.data_size):
         parser.advance()
-        if parser.commandType() == CommandType.A_COMMAND:
-            output_file.write(assemble_A(parser.symbol()))
-        elif parser.commandType() == CommandType.C_COMMAND:
+        if parser.commandType() == CommandType.L_COMMAND:
+            symbol_table.addEntry(parser.symbol(), ROM)
+        else:
+            ROM += 1
+            
+
+    # Reset parser
+    parser.current_command = None
+    parser.current_command_number = -1
+    
+
+    ### Second pass
+    # Parses input data and writes it to an output file
+    # TODO: checks for a command type two times
+    ram_var = 16
+    for i in range(parser.data_size):
+        parser.advance()
+
+        # C command
+        if parser.commandType() == CommandType.C_COMMAND:
             output_file.write(assemble_C(parser))
         
-        # TODO: L command
-        
-        else:
-            pass        
-    
-    
+        # A command
+        elif parser.commandType() == CommandType.A_COMMAND: 
+            
+            symbol = parser.symbol()
+
+            # TODO: WTF TRY TO REFACTROR THIS
+            # is number
+            try:
+                int(symbol)
+                output_file.write(assemble_A(symbol))
+                
+            # is symbol and not a number 
+            except ValueError:   
+                
+                # symbol is found
+                if symbol_table.contains(symbol):
+                    # get address of that symbol and assemble it
+                    output_file.write(assemble_A(symbol_table.getAddress(symbol)))
+                
+                # symbol is not found
+                else:
+                    # add pair to the symbol table
+                    symbol_table.addEntry(symbol, ram_var)
+                    output_file.write(assemble_A(ram_var))
+                    ram_var += 1
+            
+                
     # Closes output file
     output_file.close()
 
