c5e9ac46e0290c8245f3d9670d9dacacb8aa6ea6
DRMPN <mr.gorden.smit@gmail.com>
2021-11-04 23:48:08 +0300

    partly implemented code_writer module

--- nand2tetris/projects/07/VMTranslator/VMTranslator.py
+++ nand2tetris/projects/07/VMTranslator/VMTranslator.py
@@ -2,6 +2,11 @@
 # VM-to-Hack translator
 # xxx.vm programs -> xxx.asm programs
 # drives the process
+import os
+import sys
+from model.parser import Parser
+from model.code_writer import CodeWriter
+from model.command_type import CommandType
 
 
 # BUG FIXME HACK NOTE TODO
@@ -17,3 +22,51 @@
 # 0. Implement preprocess
 # 1. Implement the nine stack arithmetic and logical commands
 # 2. Implement the push constant x command
+
+
+def main():
+
+    # PURPOSE:  Insures correct program usage
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python3 VMTranslator.py filename.vm")
+
+    
+    # PURPOSE:  Opens given file and reads it's data
+    try:
+        file = open(sys.argv[1], 'r')
+        data = file.readlines()
+        file.close()
+    except OSError:
+        sys.exit(f'ERROR: File {sys.argv[1]} cannot be found/opened.')
+    
+
+    # Creates and initializes parser object
+    parser = Parser(data)
+
+
+    # Creates and initializes CodeWriter object
+    code_writer = CodeWriter(os.path.splitext(sys.argv[1])[0])
+
+    for i in range(parser.data_size):
+
+        parser.advance()
+
+        command_type = parser.commandType()
+
+        # TODO: write logic to parse current command
+
+        # NOTE: placeholer
+        if command_type == CommandType.C_ARITHMETIC:
+            code_writer.writeArithmetic(parser.arg1())
+        else:
+            code_writer.writePushPop(command_type.value, parser.arg1(), parser.arg2())
+
+    
+    # closes code_writer
+    code_writer.close()
+
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
--- nand2tetris/projects/07/VMTranslator/model/code_writer.py
+++ nand2tetris/projects/07/VMTranslator/model/code_writer.py
@@ -1,39 +1,57 @@
 # CodeWriter module
 # writes the assembly code that implements the parsed command
+import sys
 from typing import IO
 
 
 # PURPOSE:  Generates assembly code from the parsed VM command
-class codeWriter():
+class CodeWriter():
     # NOTE: Leaves comment for each command line that is being translated
 
     # PURPOSE:  Opens the input file/stream and gets ready to write into it.
     # constructor (file/stream)
-    def __init__(self) -> None:
-        pass
+    def __init__(self, filename) -> IO:
+        
+        self.file = None
+        
+        try:
+            self.file = open(f'{filename}.asm', 'w')
+        except OSError:
+            sys.exit(f'ERROR: File {filename} cannot be opened/created.')
+            
+        self.setFileName(filename)
 
 
     # PURPOSE:  Informes the code writer that the translation of a new VM
     #           file is started.
     def setFileName(self, filename: str) -> None:
-        pass
+        print(f'Translation has been started.\n{filename}.vm --> {filename}.asm')
 
 
     # PURPOSE:  Writes to the output file the assembly code that implements
     #           the given arithmetic command.
     # writeArithmetic (command(string))
+
+    # TODO: check out command argument
+
     def writeArithmetic(self, command: str) -> IO:
-        pass
+        self.file.write(f'// {command}\n')
+        self.file.write(f'{command}\n')
     
 
     # PURPOSE:  Writes to the output file the assembly code that implements
     #           the given command, where command is either C_PUSH or C_POP.
     # writePushPop ( command( C_PUSH or C_POP, segment(string), index(int) ) )
+
+    # TODO: check out command argument
+
     def writePushPop(self, command, segment: str, index: int) -> IO:
-        pass
+        self.file.write(f'// {command} {segment} {index} \n')
+        self.file.write(f'{command}\n')
 
 
     # PURPOSE: Closes the output file.
     # close
     def close(self) -> None:
-        pass
+        self.file.close()
+        print('Done.')
