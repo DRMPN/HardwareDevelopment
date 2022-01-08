8130cf99a3393d05fd207f50be0915f416dcd6ef
DRMPN <mr.gorden.smit@gmail.com>
2022-01-08 23:19:10 +0300

    initial commit

---
+++ nand2tetris/projects/10/SyntaxAnalyzer/JackTokenizer.py
@@ -0,0 +1,20 @@
+# Removes all comments and white space from the input stream 
+# and breaks it into Jack-language tokens, as specified by the Jack grammar.
+
+# constructor
+
+# hasMoreTokens
+
+# advance
+
+# tokenType
+
+# keyword
+
+# symbol
+
+# identifyer
+
+# intVal
+
+# stringVal
