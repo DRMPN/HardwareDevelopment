43c23f5b1226e5be5386e2ab7752ed2d96fe842a
DRMPN <mr.gorden.smit@gmail.com>
2022-01-09 02:44:45 +0300

    raw design

--- nand2tetris/projects/10/SyntaxAnalyzer/JackTokenizer.py
+++ nand2tetris/projects/10/SyntaxAnalyzer/JackTokenizer.py
@@ -1,13 +1,53 @@
+
+
 # Removes all comments and white space from the input stream 
 # and breaks it into Jack-language tokens, as specified by the Jack grammar.
+class JackTokenizer():
 
-# constructor
 
-# hasMoreTokens
+    # NOTE: create dispose() method to close input file
 
-# advance
 
-# tokenType
+    # Opens the input file/stream and gets ready to tokenize it.
+    def __init__(self) -> None:
+
+        # TODO: open input file
+        #       read data
+        #       close file
+        #       create new file .xml
+
+        # try:
+        #   self.file = open(abs_path_to_file, 'r')
+        # except OSError:
+        #   sys.exit()
+
+        self.current_token = None
+    
+    
+    # PURPOSE:  Is there more tokens in the input?
+    # RETURNS:  bool
+    def hasMoreTokens() -> bool:
+        # TODO: https://pynative.com/python-read-specific-lines-from-a-file/
+        #       https://docs.python.org/3/library/functions.html?highlight=enumerate#enumerate
+        pass
+
+
+    # PURPOSE:  Gets the next token from the input and makes it current token.
+    # NOTE: This method should only be called if hasMoreTokens() is true.
+    #       Initially there is no current token. 
+    def advance() -> None:
+        # TODO: preprocess data
+        #       ignore comments - // or /* */ or ... code ... // comment
+        #       etc
+        pass
+
+
+    # PURPOSE:  Returns the type of current token.
+    # RETURNS:  token_type
+    def tokenType() -> None:
+        # TODO:
+        #       class token_type(Enum): ...
+        pass
 
 # keyword
 
